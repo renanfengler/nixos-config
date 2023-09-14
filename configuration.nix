8dd845dd1b5688dc43714508c245f8c4ab1a930e
Renan Augusto Fengler <renan_fengler_93@hotmail.com>
2023-09-14 18:21:15 -0300

    Instalar eww e identar arquivo de configuracao
    Signed-off-by: Renan Augusto Fengler <renan_fengler_93@hotmail.com>

--- configuration.nix
+++ configuration.nix
@@ -5,187 +5,187 @@
 { config, pkgs, lib, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
-  imports = [ ./hardware-configuration.nix ];
+    nixpkgs.config.allowUnfree = true;
+    imports = [ ./hardware-configuration.nix ];
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+    nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # Permite executar binários "normais" no nix
-  # https://github.com/Mic92/nix-ld
-  # https://unix.stackexchange.com/questions/522822/different-methods-to-run-a-non-nixos-executable-on-nixos/522823#522823
-  programs.nix-ld.enable = true;
+# Permite executar binários "normais" no nix
+# https://github.com/Mic92/nix-ld
+# https://unix.stackexchange.com/questions/522822/different-methods-to-run-a-non-nixos-executable-on-nixos/522823#522823
+    programs.nix-ld.enable = true;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+# Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+    networking.hostName = "nixos"; # Define your hostname.
+    networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  time.timeZone = "America/Sao_Paulo";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "br-abnt2";
-  };
-
-  fonts = {
-    fontDir.enable = true;
-    fonts = with pkgs; [
-       siji
-       (nerdfonts.override { fonts = [ "Meslo" ]; })
-    ];
-  };
-
-  programs.hyprland = {
-      enable = true;
-  };
-
-  programs.xss-lock = {
-    enable = true;
-    lockerCommand = "${pkgs.betterlockscreen}/bin/betterlockscreen";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  users = {
-    defaultUserShell = pkgs.zsh;
-    users.renan = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" "disk" "docker" "audio" "video" "input" "systemd-journal" "networkmanager" "network" ];
+    time.timeZone = "America/Sao_Paulo";
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+        font = "Lat2-Terminus16";
+        keyMap = "br-abnt2";
     };
-  };
 
-  environment.systemPackages = with pkgs; [
-    # General
-    acpi
-    bat
-    curl
-    exa
-    fd
-    fzf
-    hyprpaper
-    openvpn
-    ripgrep
-    sudo
-    unzip
-    wget
-    wofi
-    wl-clipboard
+    fonts = {
+        fontDir.enable = true;
+        fonts = with pkgs; [
+            siji
+            (nerdfonts.override { fonts = [ "Meslo" ]; })
+        ];
+    };
 
-    # Git
-    git
-    gh
-    yadm
+    programs.hyprland = {
+        enable = true;
+    };
 
-    # GNU Compiler Collection and more stuff
-    libgccjit
-    gnumake
-    llvmPackages_16.stdenv
-    gcc13Stdenv
-    gcc
+    programs.xss-lock = {
+        enable = true;
+        lockerCommand = "${pkgs.betterlockscreen}/bin/betterlockscreen";
+    };
 
-    # Apps
-    firefox
+# Enable CUPS to print documents.
+    services.printing.enable = true;
 
-    # Terminal/Shell
-    kitty
-    tmux
-    zsh
-    zsh-autosuggestions
-    zsh-powerlevel10k
-    zsh-syntax-highlighting
+# Enable sound.
+    sound.enable = true;
+    hardware.pulseaudio.enable = true;
 
-    # Editors
-    neovim
-    vim
-    helix
+    users = {
+        defaultUserShell = pkgs.zsh;
+        users.renan = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" "disk" "docker" "audio" "video" "input" "systemd-journal" "networkmanager" "network" ];
+        };
+    };
 
-    # Programming
-    nodejs
-    python3
-    rustup
+    environment.systemPackages = with pkgs; [
+# General
+        acpi
+        bat
+        curl
+        exa
+        fd
+        fzf
+        hyprpaper
+        openvpn
+        ripgrep
+        sudo
+        unzip
+        wget
+        wofi
+        wl-clipboard
 
-    # Customization
-    playerctl
-    betterlockscreen
-    polybar
-  ];
+# Git
+        git
+        gh
+        yadm
 
-  environment.variables = {
-    EDITOR = "nvim";
-  };
+# GNU Compiler Collection and more stuff
+        libgccjit
+        gnumake
+        llvmPackages_16.stdenv
+        gcc13Stdenv
+        gcc
 
-  programs.zsh = {
-      enable = true;
-      interactiveShellInit = ''
-          source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-          source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-          source ${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
-          if [ -f ~/.zsh_aliases ]; then
-              . ~/.zsh_aliases
-          fi
-      '';
-      ohMyZsh = {
-          enable = true;
-          plugins = [
-              "autojump"
-              "colored-man-pages"
-              "copybuffer"
-              "copyfile"
-              "copypath"
-              "dirhistory"
-              "extract" 
-              "fancy-ctrl-z"
-              "fd"
-              "fzf"
-              "git"
-              "ripgrep"
-              "rust"
-              "tmux"
-          ];
-      };
-  };
+# Apps
+        firefox
 
-  programs.autojump.enable = true;
-  
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+# Terminal/Shell
+        kitty
+        tmux
+        zsh
+        zsh-autosuggestions
+        zsh-powerlevel10k
+        zsh-syntax-highlighting
 
-  # List services that you want to enable:
+# Editors
+        neovim
+        vim
+        helix
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+# Programming
+        nodejs
+        python3
+        rustup
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+# Customization
+        playerctl
+        betterlockscreen
+        polybar
+        eww
+    ];
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+    environment.variables = {
+        EDITOR = "nvim";
+    };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+    programs.zsh = {
+        enable = true;
+        interactiveShellInit = ''
+            source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+            source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+            source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+            source ${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/zsh-interactive-cd/zsh-interactive-cd.plugin.zsh
+            if [ -f ~/.zsh_aliases ]; then
+                . ~/.zsh_aliases
+            fi
+        '';
+        ohMyZsh = {
+            enable = true;
+            plugins = [
+                "autojump"
+                "colored-man-pages"
+                "copybuffer"
+                "copyfile"
+                "copypath"
+                "dirhistory"
+                "extract" 
+                "fancy-ctrl-z"
+                "fd"
+                "fzf"
+                "git"
+                "ripgrep"
+                "rust"
+                "tmux"
+            ];
+        };
+    };
+
+    programs.autojump.enable = true;
+
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+    services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+# Copy the NixOS configuration file and link it from the resulting system
+# (/run/current-system/configuration.nix). This is useful in case you
+# accidentally delete configuration.nix.
+    system.copySystemConfiguration = true;
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It's perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "23.05"; # Did you read the comment?
 }
 
