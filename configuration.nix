95322f7fce10e5a6f4d30145c40b4546069e7968
Renan Augusto Fengler <renan_fengler_93@hotmail.com>
2024-09-21 11:43:51 -0300

    Instalar coisas da nvidia
    Signed-off-by: Renan Augusto Fengler <renan_fengler_93@hotmail.com>

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,17 @@
 
     nix.settings.experimental-features = ["nix-command" "flakes"];
 
+    hardware.opengl.enable = true;
+    services.xserver.videoDrivers = ["nvidia"];
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+        open = false;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
 # Use the systemd-boot EFI boot loader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
@@ -172,6 +183,7 @@
         nspr.out
         nss.out
         util-linux
+        egl-wayland
 
 # Apps
         firefox
