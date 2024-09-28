2a3b073130e779845f1158810f3ed7efe68a32b9
Renan Augusto Fengler <renan_fengler_93@hotmail.com>
2024-09-28 15:26:29 -0300

    Instalar steam

--- configuration.nix
+++ configuration.nix
@@ -348,6 +348,10 @@
         ];
     };
 
+    programs.steam = {
+        enable = true;
+    };
+
 # Some programs need SUID wrappers, can be configured further or are
 # started in user sessions.
 # programs.mtr.enable = true;
