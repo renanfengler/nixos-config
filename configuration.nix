9ea613d29d64a40e67534230d850fcd504f35c52
Renan Augusto Fengler <renan_fengler_93@hotmail.com>
2025-11-30 06:06:44 -0300

    Adicionar bindkeys -v para modo vim do zsh

--- configuration.nix
+++ configuration.nix
@@ -278,6 +278,7 @@
         enable = true;
         autosuggestions.enable = true;
         interactiveShellInit = ''
+            bindkeys -v
             export PATH=$PATH:$HOME/.local/bin
             export PATH=$PATH:$HOME/.config/yarn/global/node_modules/.bin/
             export PATH=$PATH:$HOME/.cargo/bin/
