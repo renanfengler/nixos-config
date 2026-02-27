b9d31123e46ea051d4e25a666f04af1db5b03422
Renan Augusto Fengler <renan_fengler_93@hotmail.com>
2026-02-27 06:19:25 -0300

    Remover config do plymouth e arquivos desnecessários

--- configuration.nix
+++ configuration.nix
@@ -30,32 +30,6 @@
     };
 
     boot = {
-        plymouth = {
-            enable = true;
-            theme = "spin";
-            themePackages = with pkgs; [
-                # By default we would install all themes
-                (adi1090x-plymouth-themes.override {
-                    selected_themes = [ "spin" ];
-                })
-            ];
-        };
-
-        # Enable "Silent boot"
-        consoleLogLevel = 3;
-        initrd.verbose = false;
-        kernelParams = [
-            "quiet"
-            "splash"
-            "boot.shell_on_fail"
-            "udev.log_priority=3"
-            "rd.systemd.show_status=auto"
-        ];
-        # Hide the OS choice for bootloaders.
-        # It's still possible to open the bootloader list by pressing any key
-        # It will just not appear on screen unless a key is pressed
-        loader.timeout = 0;
-
         # Use the systemd-boot EFI boot loader.
         loader.systemd-boot.enable = true;
         loader.efi.canTouchEfiVariables = true;
